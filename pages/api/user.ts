5d56387c44dd3c84c32977ef1005208e34ed5a3b
jevinambaliya26 <jevinambaliya@gmail.com>
2023-03-16 12:03:10 +0530

    get client ip address

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,14 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import requestIp from "request-ip";
 
 type Data = {
   name: string;
   email: string;
+  ip: any;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe", email: "john123@gmail.com" });
+  const clientIp = requestIp.getClientIp(req);
+  res
+    .status(200)
+    .json({ name: "John Doe", email: "john123@gmail.com", ip: clientIp });
 }
